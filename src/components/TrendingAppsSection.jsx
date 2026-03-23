d521209ee40fb17417d0d4e09f6232224b7f8007
SaikatKarar <kararsaikat@gmail.com>
2026-03-23 23:20:17 +0530

    Make App Details Page

--- src/components/TrendingAppsSection.jsx
+++ src/components/TrendingAppsSection.jsx
@@ -1,63 +1,6 @@
 import { FaArrowTrendUp, FaDownload, FaStar } from "react-icons/fa6";
-
-const appsData = [
-    {
-        id: 1,
-        name: "Forest: Focus for Productivity",
-        image: "/public/icon-005.webp",
-        downloads: "7M",
-        rating: "4.9",
-    },
-    {
-        id: 2,
-        name: "SmPlan: ToDo List with Reminder",
-        image: "/public/icon-006.webp",
-        downloads: "8M",
-        rating: "4.9",
-    },
-    {
-        id: 3,
-        name: "FLIP - Focus Timer for Study",
-        image: "/public/icon-007.webp",
-        downloads: "3M",
-        rating: "4.3",
-    },
-    {
-        id: 4,
-        name: "Pomocat - Cute Pomodoro Timer",
-        image: "/public/icon-008.webp",
-        downloads: "5M",
-        rating: "4.0",
-    },
-    {
-        id: 5,
-        name: "Time Planner: Schedule & Tasks",
-        image: "/public/icon-009.webp",
-        downloads: "4M",
-        rating: "4.3",
-    },
-    {
-        id: 6,
-        name: "Flip Clock: World Clock",
-        image: "/public/icon-010.webp",
-        downloads: "9M",
-        rating: "4.0",
-    },
-    {
-        id: 7,
-        name: "Morning Habits - Daily Routine",
-        image: "/public/icon-011.webp",
-        downloads: "3M",
-        rating: "4.2",
-    },
-    {
-        id: 8,
-        name: "Focus Plant: Pomodoro Forest",
-        image: "/public/icon-012.webp",
-        downloads: "7M",
-        rating: "4.4",
-    },
-];
+import { Link } from "react-router-dom";
+import apps from "../data/apps.json";
 
 export default function TrendingAppsSection() {
     return (
@@ -76,23 +19,23 @@ export default function TrendingAppsSection() {
 
                 {/* Grid */}
                 <div className="w-11/12 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
-                    {appsData.map((app) => (
-                        <a
+                    {apps.slice(0, 8).map((app) => (
+                        <Link
                             key={app.id}
-                            href={`/apps/${app.id}`}
+                            to={`/apps/${app.id}`}
                             className="bg-white p-4 rounded-2xl shadow-lg hover:-translate-y-2 hover:shadow-xl transition-all duration-300 flex flex-col gap-4"
                         >
                             {/* Image */}
                             <div>
                                 <img
                                     src={app.image}
-                                    alt={app.name}
+                                    alt={app.title}
                                     className="w-full h-full object-cover rounded-xl"
                                 />
                             </div>
 
-                            {/* Name */}
-                            <p className="font-semibold text-center">{app.name}</p>
+                            {/* Title */}
+                            <p className="font-semibold text-center">{app.title}</p>
 
                             {/* Stats */}
                             <div className="flex justify-between">
@@ -105,21 +48,21 @@ export default function TrendingAppsSection() {
                                 {/* Rating */}
                                 <div className="flex items-center gap-1 text-purple-600 text-sm font-medium bg-purple-100 px-2 py-1 rounded-full">
                                     <FaStar />
-                                    {app.rating}
+                                    {app.ratingAvg}
                                 </div>
                             </div>
-                        </a>
+                        </Link>
                     ))}
                 </div>
 
                 {/* Button */}
                 <div className="text-center">
-                    <a
-                        href="/apps"
+                    <Link
+                        to="/apps"
                         className="px-8 py-3 rounded-full text-white bg-gradient-to-tl from-[#9F62F2] to-[#632EE3] hover:shadow-xl transition"
                     >
                         Show All
-                    </a>
+                    </Link>
                 </div>
             </div>
         </section>
--- src/pages/AppDetails.jsx
+++ src/pages/AppDetails.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import apps from "../data/apps.json";
 
-function AppDetails() {
-    return (
-        <div>AppDetails</div>
-    )
+import { FaDownload, FaStar } from "react-icons/fa6";
+import { MdReviews } from "react-icons/md";
+
+export default function AppDetails() {
+  const { id } = useParams();
+  const app = apps.find((item) => item.id === Number(id));
+
+  if (!app) {
+    return <p className="text-center py-20">App Not Found</p>;
+  }
+
+  return (
+    <main className="min-h-screen w-11/12 mx-auto py-20 space-y-12">
+
+      <div className="flex flex-col lg:flex-row gap-12 items-center">
+
+        <div className="flex-1 flex justify-center">
+          <div className="bg-gray-100 p-6 rounded-2xl shadow-lg w-full max-w-sm">
+            <img
+              src={app.image}
+              alt={app.title}
+              className="rounded-xl w-full object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="flex-1 space-y-6">
+
+          <div>
+            <h2 className="text-4xl font-bold text-gray-800">
+              {app.title}
+            </h2>
+            <p className="mt-2 text-gray-500">
+              Developed by{" "}
+              <span className="text-indigo-600 font-semibold cursor-pointer hover:underline">
+                {app.companyName}
+              </span>
+            </p>
+          </div>
+          <hr className="text-gray-300" />
+          <div className="flex flex-wrap gap-10 items-center">
+
+            <div className="flex flex-col  gap-3">
+              <FaDownload className="text-green-500 text-2xl" />
+              <div>
+                <p className="text-gray-500 text-sm">Downloads</p>
+                <p className="text-xl font-bold">{app.downloads}</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col  gap-3">
+              <FaStar className="text-orange-400 text-2xl" />
+              <div>
+                <p className="text-gray-500 text-sm">Average Ratings</p>
+                <p className="text-xl font-bold">{app.ratingAvg}</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col  gap-3">
+              <MdReviews className="text-purple-500 text-2xl" />
+              <div>
+                <p className="text-gray-500 text-sm">Total Reviews</p>
+                <p className="text-xl font-bold">{app.reviews}</p>
+              </div>
+            </div>
+
+          </div>
+
+          <button className="bg-green-500 hover:bg-green-600 text-white px-8 py-3 rounded-lg shadow-md transition font-semibold">
+            Install Now ({app.size} MB)
+          </button>
+
+        </div>
+      </div>
+
+      <hr className="text-gray-300" />
+      <div>
+        <h2 className="text-3xl font-bold mb-6">Ratings</h2>
+
+        <div className="space-y-4">
+
+          {app.ratings.map((item, index) => {
+            const max = Math.max(...app.ratings.map(r => r.count));
+            const percentage = (item.count / max) * 100;
+
+            return (
+              <div key={index} className="flex items-center gap-4">
+
+                <p className="w-12 text-gray-600 text-sm">
+                  {item.name}
+                </p>
+
+                <div className="flex-1 h-4 overflow-hidden">
+                  <div
+                    className="h-full bg-orange-500 transition-all duration-500"
+                    style={{ width: `${percentage}%` }}
+                  ></div>
+                </div>
+
+              </div>
+            );
+          })}
+
+        </div>
+
+        <div className="mt-6 flex justify-between text-xs text-gray-500 px-12">
+          <span>0</span>
+          <span>3000</span>
+          <span>6000</span>
+          <span>9000</span>
+          <span>12000</span>
+        </div>
+      </div>
+
+      <hr className="text-gray-300" />
+
+      <div>
+        <h2 className="text-3xl font-bold mb-5">Description</h2>
+        <p className="text-gray-600 leading-relaxed">
+          {app.description}
+        </p>
+      </div>
+
+    </main>
+  );
 }
-
-export default AppDetails
